b4a00538a7e6bb0aadfcd882ac054b7f08a55f00
Luis Felipe Roque <161150723+luisfelipeaz@users.noreply.github.com>
2025-04-08 10:11:51 -0300

    Add files via upload

---
+++ atividade_2/src/main/java/com/mycompany/horadosistema/HoraDoSistema.java
@@ -0,0 +1,12 @@
+package com.mycompany.horadosistema;
+
+import java.util.Date;
+
+public class HoraDoSistema {
+
+    public static void main(String[] args) {
+        Date data = new Date();
+        System.out.println("A hora do sistema e:");
+        System.out.println(data.toString());
+    }
+}
